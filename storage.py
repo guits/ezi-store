49f1ce8233176663a30ebc842009ca69e1b5b89a
Guits <guillaume.abrioux@enovance.com>
2015-03-09 22:20:46 +0100

    daily backup

--- storage.py
+++ storage.py
@@ -5,7 +5,6 @@ from sqlalchemy.orm import relationship, sessionmaker
 Base = declarative_base()
 engine = create_engine('sqlite:///ezistore.db', echo=False)
 
-# {'dummy': u'', 'keyid': u'904F1ABA14E52AB0', 'expires': u'1457164543', 'subkeys': [], 'length': u'4096', 'ownertrust': u'u', 'algo': u'1', 'fingerprint': u'9126B6976BA0178FEDC909BC904F1ABA14E52AB0', 'date': u'1425628543', 'trust': u'u', 'type': u'pub', 'uids': [u'ezi_store_srv (ezi-store server key) <ezi-store@zigzag.sx>']}
 
 class Setup(Base):
     __tablename__ = 'setup'
@@ -16,44 +15,63 @@ class Setup(Base):
     fingerprint = Column(String(250))
     type = Column(String(3))
     uids = Column(String(250))
- 
+
+    def __init__(self, keyid, expires, length, fingerprint, type, uids):
+        self.keyid = keyid
+        self.expires = expires
+        self.length = length
+        self.fingerprint = fingerprint
+        self.type = type
+        self.uids = uids
+
 Base.metadata.create_all(engine)
 
 
-class Storage(object):
-    def __init__(self):
-        Session = sessionmaker(bind=engine)
-        session = Session()
-
-
- 
-# Create an artist
-new_artist = Artist("Newsboys")
-new_artist.albums = [Album("Read All About It", 
-                           datetime.date(1988,12,01),
-                           "Refuge", "CD")]
- 
-# add more albums
-more_albums = [Album("Hell Is for Wimps",
-                     datetime.date(1990,07,31),
-                     "Star Song", "CD"),
-               Album("Love Liberty Disco", 
-                     datetime.date(1999,11,16),
-                     "Sparrow", "CD"),
-               Album("Thrive",
-                     datetime.date(2002,03,26),
-                     "Sparrow", "CD")]
-new_artist.albums.extend(more_albums)
- 
-# Add the record to the session object
-session.add(new_artist)
-# commit the record the database
+Session=sessionmaker(bind=engine)
+session = Session()
+new_key = Setup(keyid = '123', expires = '21/02/00', length='1024', fingerprint='aa:cc', type='RSA', uids='foo')
+session.add(new_key)
 session.commit()
- 
-# Add several artists
-session.add_all([
-    Artist("MXPX"),
-    Artist("Kutless"),
-    Artist("Thousand Foot Krutch")
-    ])
-session.commit()
+
+res = session.query(Setup).all()
+print res
+for r in res:
+    print r.keyid, r.id
+
+#class Storage(object):
+#    def __init__(self):
+#        Session = sessionmaker(bind=engine)
+#        session = Session()
+#
+#
+# 
+## Create an artist
+#new_artist = Artist("Newsboys")
+#new_artist.albums = [Album("Read All About It", 
+#                           datetime.date(1988,12,01),
+#                           "Refuge", "CD")]
+# 
+## add more albums
+#more_albums = [Album("Hell Is for Wimps",
+#                     datetime.date(1990,07,31),
+#                     "Star Song", "CD"),
+#               Album("Love Liberty Disco", 
+#                     datetime.date(1999,11,16),
+#                     "Sparrow", "CD"),
+#               Album("Thrive",
+#                     datetime.date(2002,03,26),
+#                     "Sparrow", "CD")]
+#new_artist.albums.extend(more_albums)
+# 
+## Add the record to the session object
+#session.add(new_artist)
+## commit the record the database
+#session.commit()
+# 
+## Add several artists
+#session.add_all([
+#    Artist("MXPX"),
+#    Artist("Kutless"),
+#    Artist("Thousand Foot Krutch")
+#    ])
+#session.commit()
