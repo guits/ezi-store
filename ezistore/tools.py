13160586a7b8ccf8fb02b0446851d1e47152bbf3
Guits <guillaume.abrioux@enovance.com>
2015-03-26 10:45:32 +0100

    Add tools.py

---
+++ ezistore/tools.py
@@ -0,0 +1,14 @@
+def merge(a, b, path=None):
+    # merges b into a
+    if path is None: path = []
+    for key in b:
+        if key in a:
+            if isinstance(a[key], dict) and isinstance(b[key], dict):
+                merge(a[key], b[key], path + [str(key)])
+            elif a[key] == b[key]:
+                pass
+            else:
+                a[key] = b[key]
+        else:
+            a[key] = b[key]
+    return a
