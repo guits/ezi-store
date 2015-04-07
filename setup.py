379d3e5d326fac30d8e961c19170bf8cbef607b3
Guits <guillaume.abrioux@enovance.com>
2015-04-07 17:37:30 +0200

    Bump

---
+++ setup.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+
+from setuptools import setup
+
+setup(name='ezi-store',
+      version='0.1',
+      description=('This is a very simple password keeper based on sqlite storage '
+                   'and asymmetric gpg encryption'),
+      author='guits',
+      author_email='guits@zigzag.sx',
+      url='https://github.com/guits/ezi-store',
+      packages=['ezi-store'],
+      scripts=['ezistored.py'],
+     )
