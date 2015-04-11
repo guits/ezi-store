cb183f831aeee740811c3cfb76c2e91369127451
Guits <guillaume.abrioux@enovance.com>
2015-04-11 07:37:07 +0200

    Move Core() class to separate file

--- ezistore/core.py
+++ ezistore/core.py
@@ -1,16 +1,4 @@
-#!/usr/bin/python
-
-import string, sys
-import re
-import subprocess
-import logging
-import datetime
-import argparse
-from ezistore.storage import *
-from ezistore.config import *
-from ezistore.gpg import *
 from ezistore.server import *
-
 
 class Core(object):
     def __init__(self, configuration):
@@ -19,45 +7,4 @@ class Core(object):
     def run(self):
         for i in self._server.getmessage():
             print i
-
-
-def ezistored():
-    parser = argparse.ArgumentParser()
-    parser.add_argument("-c", "--config", help="config file",
-                        required=True, action="store", metavar="FILE")
-    args = parser.parse_args()
-    if args.config:
-       configfilename=args.config
-    else:
-       configfilename='/opt/ezi-store/config'
-
-    default_config = {'server': {
-                                  'bind_address': '127.0.0.1',
-                                  'bind_port': '40000'},
-                      'logging': {
-                                  'logfilename': '/var/log/ezistore'},
-                      'gpg': {
-                              'gnupghome': '/opt/ezi-store/gpg',
-                              'key_type': 'RSA',
-                              'key_length': '4096',
-                              'expire_date': '365d',
-                              'name_real': 'Ezi Store',
-                              'name_email': 'ezi-store@zigzag.sx',
-                              'name_comment': 'ezi-store server key'}
-                     }
-    config = Config(configfilename)
-    if config == None:
-        #TODO: Make something better
-        print "Can't merge configuration"
-        return None
-    merged_config = config.load(default_config)
-    init_log(filename=merged_config['logging']['logfilename'])
-    
-#    gpg = Gpg(configuration = merged_config)
-#    gpg.srv_pub_key_exist()
-#    gpg.gen_keys()
-#    gpg.list_keys()
-#    gpg.export_armored_srv_pub_key('CA9338C386BE60FC')
-    exit(1)
-    core = Core(configuration = merged_config)
-    core.run()
+-
