9e0cad98dc1e60ae22d6d4e1c0bad88bd99e44da
Guits <guillaume.abrioux@enovance.com>
2015-04-14 10:23:34 +0200

    backup

--- ezistore/core.py
+++ ezistore/core.py
@@ -4,13 +4,13 @@ from ezistore.gpg import *
 class Core(object):
     def __init__(self, configuration):
         self._server = Server(configuration)
+        self._gpg = Gpg(configuration = configuration)
 
     def run(self):
         for i in self._server.getmessage():
             print i
-#    gpg = Gpg(configuration = merged_config)
-#    gpg.srv_pub_key_exist()
-#    gpg.gen_keys()
-#    gpg.list_keys()
-#    gpg.export_armored_srv_pub_key('CA9338C386BE60FC')
-#    exit(1)
+#        gpg.srv_pub_key_exist()
+#        gpg.gen_keys()
+#        gpg.list_keys()
+#        gpg.export_armored_srv_pub_key('CA9338C386BE60FC')
+#        exit(1)
